db8c820f192b171489189bf5f5c8bb049b7d6a94
samueldanso <me.samueldanso@gmail.com>
2025-05-08 05:54:47 +0000

    feat: initial setup Next App and Shadcn/ui

---
+++ src/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "@/styles/globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Believr – The social app for early believers to co-invest in creators ",
+  description:
+    "Believr is a decentralized social app for early believers that allows fans to co-invest in creators they believe in. Built on Lens Protocol, it helps creators raise funds and build loyal communities, while fans earn rewards and unlock exclusive access.",
+  icons: [{ rel: "icon", url: "/favicon.svg" }],
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Are you a Believr?</h1>
+    </div>
+  );
+}
